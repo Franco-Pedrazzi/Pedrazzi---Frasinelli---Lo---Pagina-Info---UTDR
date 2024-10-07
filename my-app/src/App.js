2a57849f4d39fd24bab691c8076f0274a1d3942d
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-10-07 16:58:05 -0300

    agregue detalles visuales

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './index.css'; // Import the CSS file with the @font-face rule
 import titulo from './files/Images/Titulo.png'
 const App = () => {
+  //<iframe src="https://jcw87.github.io/c2-sans-fight/" name="Game Name" width="800" height="480" frameborder="0" scrolling="no" ><p>Your browser does not support iframes.</p></iframe>
   return (
-    <div style={{textAlign:'center'}}>
+    <div>
+      <nav>
+        <div className='button'>Undertale </div>
+        <div className='button'>Deltarune </div>
+      </nav>
+      <center>
       <img src={titulo}></img>
       <p>by toby fox</p>
-    </div>
+
+      </center>
+      
+      </div> 
   );
 };
 
