d5b94bb7620fbc3b37b2b160d9d14be562b6572a
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-10-16 11:27:33 -0300

    agregue info

--- my-app/src/components/IndexUndertale.jsx
+++ my-app/src/components/IndexUndertale.jsx
@@ -11,27 +11,11 @@ import Annoying_dog from '../files/Images/Annoying dog.png';
 import quit from '../files/Images/Quit.png'
 import loading from '../files/Images/Annoying_Dog_overworld_sleeping.webp';
 import intro from '../files/Images/undertale_intro.mp4'
-
+import Determination_soul from '../files/Images/detemination soul.png'
+import monster_soul from '../files/Images/monster soul.png'
 function IndexUndertale() {
   const [docData, setDocData] = useState(null);
   const [introduccion, setIntroduccion] = useState(false);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const docRef = doc(db, 'Undertale', 'UT');
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setDocData(docSnap.data());
-        } else {
-          console.log("No se encontró el documento");
-        }
-      } catch (error) {
-
-        console.error("Error al obtener el documento:", error);
-      }
-    }
-    fetchData()
-  }, []);
 
   return (
     <div>
@@ -55,6 +39,53 @@ function IndexUndertale() {
             <div>
               <img src={titulo} alt="Titulo" className='titulo' />
               <p>by Toby Fox</p>
+              <h3>
+                Undertale es un RPG en el que controlas a Frisk, un humano, que tras subir el monte Ebott caería a un mundo repleto de monstruos, llamado Underground y ahora ese mundo estará destinado a estar bajo tu control y tu fuerte <determinacion>Determinación</determinacion> ya que no importa lo que hagan los habitantes, tu <determinacion>Determinación</determinacion> te hace imparable.
+              </h3>
+              <br />
+              <h1>RUTAS</h1>
+              <h3>
+                Undertale tiene 3 Rutas la  <neutral>Neutral</neutral>,la <pacifista>Pacifista</pacifista> y la <genocida>Genocida</genocida> que serían las maneras en las que te irás del Underground , cada ruta depende de tu relación con los monstruos
+              </h3>
+              <h3>
+                La ruta <neutral>Neutral</neutral> es cuando te haces amigo de algunos monstruos pero matas otros, solo con matar UN monstruo ya se inicia esta ruta, esta ruta tiene varios finales dependiendo de a quien mates o de quien te hagas amigo.
+              </h3>
+              <h3>
+                La ruta <pacifista>Pacifista</pacifista> es cuando te haces amigo de todos los monstruos y no matas a ninguno
+              </h3>
+              <h3>
+                La ruta <genocida>Genocida</genocida> es cuando <genocida>MATAS A TODOS SOLO DEJANDO SU POLVO</genocida>.
+              </h3>
+              <h1>Almas</h1>
+              <h3>En Undertale existe el Alma que es la culminación de cada ser, el Alma se hace más fuerte con <genocida>LOVE</genocida>(LV) y aumenta con una determinada cantidad de <genocida>EXP</genocida>.              </h3>
+              <h3>¿Qué significa  <genocida>LOVE</genocida>? podrías pensar que significa amor en ingles pero no, es un acrónimo de <genocida>Level of Violence</genocida> (Nivel de Violencia), ¿y <genocida>EXP</genocida>? es otro acrónimo de <genocida>Execution Points</genocida> (Puntos de Exterminio) y se van ganando mientras vas matando Monstruos </h3>
+
+              <h3>Los humanos pueden tener hasta 7 diferentes tipos de Almas</h3>
+              <div className='almas'>
+                <paciencia><img src={Determination_soul} /> Paciencia</paciencia>
+                <br />
+                <valentia><img src={Determination_soul} /> Valentía</valentia>
+                <br />
+                <integridad><img src={Determination_soul} /> Integridad</integridad>
+                <br />
+                <perseverancia><img src={Determination_soul} /> Perseverancia</perseverancia>
+                <br />
+                <amabilidad><img src={Determination_soul} /> Amabilidad</amabilidad>
+                <br />
+                <justicia><img src={Determination_soul} /> Justicia</justicia>
+                <br />
+                <determinacion><img src={Determination_soul} /> Determinación</determinacion>(la de nuestro protagonista)
+              </div>
+              <h1>Los Monstruos</h1>
+              <h3>Los monstruos a diferencia de los humanos su cuerpo está hecho con magia, haciendo de que ellos puedan hacer poderosos ataques mágicos pero aun asi los humanos son más poderosos, ya que los cuerpos de los monstruos y sus Almas son más débiles que las de un humano, tan haci que cuando mueren su cuerpo se hace polvo y se alma pedazos, todos los monstruos a excepción de uno su Rey <determinacion>Asgore</determinacion> ya que su alma aunque él muera se mantendrá intacta.
+              <h3>asi se ve el alma de los monstruos</h3>
+              <img style={{width:"20%",height:"20%"}}src={monster_soul}></img>
+              </h3>
+              <br />
+<h2>¿Por qué los humanos encerraron a los monstruos en el Underground si eran más débiles que ellos?</h2>
+           <h3>
+Eso Tiene una simple respuesta <genocida>MIEDO</genocida> por que cuando un Humano muere su alma se mantiene y los monstruos puede absorber y hacerse mas poderosos de tal modo que con 7 se vuelven <genocida>DIOSES</genocida>
+</h3>
             </div>
           ) : (
             <div>
@@ -62,16 +93,6 @@ function IndexUndertale() {
               <div onClick={() => setIntroduccion(true)} ><img className='button' src={quit}></img></div>
             </div>
 
-          )}
-          {docData ? (
-            <div>
-              {introduccion && <h3>{docData["Introducción"]}</h3>}
-
-            </div>
-          ) : (
-            <div>
-              {introduccion && <img src={loading} alt="Loading..." />}
-            </div>
           )}
         </div>
       </center>
